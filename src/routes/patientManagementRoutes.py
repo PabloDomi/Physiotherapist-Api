4c8f0a65360e43b1985beb82b58b67dadd75ece4
PabloDomi <domi@usal.es>
2024-04-29 15:09:09 +0200

    ruta registrar pacientes

--- src/routes/patientManagementRoutes.py
+++ src/routes/patientManagementRoutes.py
@@ -23,13 +23,22 @@ class RegisterPatient(Resource):
     @patient_management_ns.expect(patient_input_model)
     @patient_management_ns.marshal_list_with(success_model)
     def post(self):
-        patient = Patient(
-            name=patient_management_ns.payload["name"],
-            surname=patient_management_ns.payload["surname"],
-            age=patient_management_ns.payload["age"],
-            gender=patient_management_ns.payload["gender"],
-            routine_id=patient_management_ns.payload["routine_id"]
-        )
+
+        if patient_management_ns.payload["routine_id"] == 'Ninguna':
+            patient = Patient(
+                name=patient_management_ns.payload["name"],
+                surname=patient_management_ns.payload["surname"],
+                age=patient_management_ns.payload["age"],
+                gender=patient_management_ns.payload["gender"]
+            )
+        else:
+            patient = Patient(
+                name=patient_management_ns.payload["name"],
+                surname=patient_management_ns.payload["surname"],
+                age=patient_management_ns.payload["age"],
+                gender=patient_management_ns.payload["gender"],
+                routine_id=patient_management_ns.payload["routine_id"]
+            )
 
         db.session.add(patient)
         db.session.commit()
